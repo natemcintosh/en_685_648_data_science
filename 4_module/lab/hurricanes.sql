1877cca200c838de5690a0ff14115ec172dd9f52
Nathan McIntosh <Nathan.McIntosh@jhuapl.edu>
2020-06-17 10:41:01 -0400

    created empty database

---
+++ 4_module/lab/hurricanes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE atlantic_hurricanes
+(
+    year INTEGER,
+    tropical_storms INTEGER,
+    hurricanes INTEGER,
+    major_hurricanes INTEGER,
+    deaths INTEGER,
+    damage REAL,
+    notes TEXT
+);
